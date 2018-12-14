a88d81f6ecd4d1cfd434fb41c73d4d07affb89ff
Héctor Molinero Fernández <hector@molinero.xyz>
2018-12-14 19:21:36 +0100

    Improved checksums.yml.sh

--- installers/checksums.yml.sh
+++ installers/checksums.yml.sh
@@ -2,13 +2,15 @@
 
 set -eu
 
-cd "$(dirname "$(readlink -f "$0")")"
-
-printf -- '%s\n' 'installers:'
-for file in *; do
-	[ "${file#checksums.yml}" != "${file}" ] && continue;
-	printf -- '%s\n' "  - ${file}:"
-	printf -- '%s\n' "      md5: $(md5sum -- "${file}" | awk '{print $1}')"
-	printf -- '%s\n' "      sha1: $(sha1sum -- "${file}" | awk '{print $1}')"
-	printf -- '%s\n' "      sha256: $(sha256sum -- "${file}" | awk '{print $1}')"
-done
+printf 'installers:\n' && find "$(dirname "$(readlink -f "$0")")" \
+	-type f \( -name '*.exe' -or -name '*.zip' \) \
+	-exec sh -s -eu '{}' '+' <<-'EOF'
+		_IFS=$IFS; IFS="$(printf '\nx')"; IFS="${IFS%x}"
+		for file in $(printf -- '%s\n' "$@" | sort); do
+			printf -- '  - %s:\n'          "$(basename -- "${file}")"
+			printf -- '      md5: %s\n'    "$(md5sum -- "${file}" | awk '{print $1}')"
+			printf -- '      sha1: %s\n'   "$(sha1sum -- "${file}" | awk '{print $1}')"
+			printf -- '      sha256: %s\n' "$(sha256sum -- "${file}" | awk '{print $1}')"
+		done
+		IFS=$_IFS
+	EOF

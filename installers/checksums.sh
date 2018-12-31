b5e0dd366fccb29375f3bda68a46fa0d4c71d5cd
Héctor Molinero Fernández <hector@molinero.xyz>
2018-12-31 14:18:49 +0100

    Updated checksums

---
+++ installers/checksums.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+set -eu
+
+rm -f ./MD5SUMS ./SHA1SUMS ./SHA256SUMS
+find ./ \
+	-type f \
+	-not '(' \
+		-name 'checksums.sh' \
+		-or -name 'README.md' \
+		-or -name 'MD5SUMS' \
+		-or -name 'SHA1SUMS' \
+		-or -name 'SHA256SUMS' \
+	')' \
+	-exec sh -c 'md5sum "$1" >> ./MD5SUMS' _ '{}' ';' \
+	-exec sh -c 'sha1sum "$1" >> ./SHA1SUMS' _ '{}' ';' \
+	-exec sh -c 'sha256sum "$1" >> ./SHA256SUMS' _ '{}' ';'
